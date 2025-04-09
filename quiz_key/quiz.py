57316802ef21823f5f125c83ea4d735fc9e08038
hvcsc <jrhcmll13@gmail.com>
2025-04-09 08:21:48 +0800

    pseudocode

--- quiz_key/quiz.py
+++ quiz_key/quiz.py
@@ -1,1 +1,8 @@
-#
+#assign to text file
+#create list
+#ask for questions and choices
+#ask for correct answer
+#append the questions and choices
+#ask if the user wants to input more questions
+#store to text file
+#call def
