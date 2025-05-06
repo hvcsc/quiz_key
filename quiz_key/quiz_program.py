aeb2df2bf2fd2d10a0d312e27537d2617a92466a
hvcsc <jrhcmll13@gmail.com>
2025-05-06 18:18:28 +0800

    setup base quiz app structure and dependencies, after creating the python file and defining the initial class.

--- quiz_key/quiz_program.py
+++ quiz_key/quiz_program.py
@@ -1,9 +1,27 @@
 #import tkinter and random
+import tkinter as tk
+from tkinter import messagebox
+import random
+
 #define the quizgame class
-#load quiz questions
-#display quiz questions
-#display intro screen with start button
-#show question one at a time
+class QuizGame:
+    def __init__(self, root):
+    #display intro screen with start button
+        self.root = root
+        self.root.title("Object-Oriented Programming Quiz")
+        self.root.geometry("700x500")
+        self.root.config(bg="#EAE0D5")
+        #load quiz questions
+        #display quiz questions
+        #show question one at a time
+        self.score = 0
+        self.question_index = 0
+        self.questions = self.load_questions()
+        random.shuffle(self.questions)
+        self.is_paused = False
+    
+        self.create_start_screen()
+
 #read the quiz txt
 #parse each bock: question, options, and the correct answer
 #append to questions list
