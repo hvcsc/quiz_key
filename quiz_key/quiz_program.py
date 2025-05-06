3a810f92b31401b090ddc8930d6c8d570316a12b
hvcsc <jrhcmll13@gmail.com>
2025-05-06 16:59:49 +0800

    pseudocode

---
+++ quiz_key/quiz_program.py
@@ -0,0 +1,21 @@
+#import tkinter and random
+#define the quizgame class
+#load quiz questions
+#display quiz questions
+#display intro screen with start button
+#show question one at a time
+#read the quiz txt
+#parse each bock: question, options, and the correct answer
+#append to questions list
+#create centered title and start button
+#display the questions and options
+#display pause, exit, and disabled next button
+#bottom frame with controls
+#disable all answer buttons
+#green for correct, red for wrong
+#enable next button
+#pause: freeze the screen and show message
+#resume: continue the quiz
+#show congratulations message
+#display the score and add restart/exit buttons
+#launch the quiz
